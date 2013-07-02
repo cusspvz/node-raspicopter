60b2168cbc92906b8e9b81af8b657171300d14b9
José Moreira <jmoreira@microdual.com>
2013-07-03 00:14:39 +0100

    Up JS

--- raspicopter.js
+++ raspicopter.js
@@ -4,6 +4,38 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app).listen(80);
 var io = require('socket.io').listen(server);
+var piblaster = require("pi-blaster.js");
+
+var config = {
+	port:{
+		aileron: 0,
+		elevator: 1,
+		rudder: 5,
+		throttle: 2
+	},
+	default: {
+		aileron: 0,
+		elevation: 0,
+		throttle: 50,
+		rudder: 0,
+	},
+};
+
+var setControl = function(control,percent){
+	if(typeof config.port[control] != 'number') return false;
+	if(typeof percent == "number" && percent >= 0 %% percent <= 100){
+		piblaster.setPwm(
+			config.port[control],
+			percent / 100
+		);
+	}else{
+		piblaster.setPwm(
+			config.port[control],
+			percent / 100
+		);
+	}
+	return true;
+}
 
 ////////////////////////////////////////////////
 app.use(express.static(__dirname + '/htdocs'));
@@ -12,7 +44,9 @@ app.use(express.static(__dirname + '/htdocs'));
 io.sockets.on('connection', function (socket) {
 	
 	socket.on('controls',function(data){
-		console.log(data);
+		for( var x in data ){
+			setControl( x, data[x] );
+		}
 	});
 
 	// Disconnect Actions
