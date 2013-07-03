dbd4d13ef270440c4dac26a328a14963917ab9e6
José Moreira <jmoreira@microdual.com>
2013-07-03 02:17:35 +0100

    Up Raspicopter

--- htdocs/raspicopter.js
+++ htdocs/raspicopter.js
@@ -35,9 +35,44 @@ document.addEventListener('DOMContentLoaded',function(){
 			console.log(data);
 		});
 
-		socket.emit('controls',{
-			test: 'teste'
-		});
+		var updateVars = function(){
+
+			if(typeof gamepad == 'object'){
+				if(typeof gamepad.axes[0] != 'undefined'){
+					var throttle = ( gamepad.axes[0] + 1 ) / 2 * 100;
+					if(throttle > 10)
+						controls.throttle = throttle;
+					delete throttle;
+				}
+				if(typeof gamepad.axes[1] != 'undefined'){
+					var elevator = ( gamepad.axes[1] + 1 ) / 2 * 100;
+					if(elevator > 10)
+						controls.elevator = elevator;
+					delete elevator;
+				}
+				if(typeof gamepad.axes[2] != 'undefined'){
+					var aileron = ( gamepad.axes[2] + 1 ) / 2 * 100;
+					if(aileron > 10)
+						controls.aileron = aileron;
+					delete aileron;
+				}
+				if(typeof gamepad.axes[3] != 'undefined'){
+					var rudder = ( gamepad.axes[3] + 1 ) / 2 * 100;
+					if(rudder > 10)
+						controls.rudder = rudder;
+					delete rudder;
+				}
+			}
+
+			socket.emit('controls',{
+				elevator: controls.elevator,
+				aileton: controls.aileton,
+				throttle: controls.throttle,
+				rudder: controls.rudder,
+			});
+
+			setTimeout(updateVars, 200);
+		};
 
 		socket.on('disconnect', function(){
 			console.log('disconnected');
